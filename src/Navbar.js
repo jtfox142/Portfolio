230ba804138ec2abd2dd2d5b662349508be0d291
Jacob (J.T.) Fox <jtfox142@gmail.com>
2025-05-29 15:12:25 -0500

    Converted navbar to flexbox instead of using float

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,10 +4,11 @@ import './App.css'
 const Navbar = () => {
     
   return (
-    <div className="App">
+    <div className="navBarContainer">
       <ul className='navBar'>
         <li className='navTitle'>Jacob "JT" Fox</li>
-        <li className='navItem'>About</li>
+        <li className='navSpacer' />
+        <li className='navItem'>About Me</li>
         <li className='navItem'>Skills</li>
         <li className='navItem'>All Projects</li>
       </ul>
